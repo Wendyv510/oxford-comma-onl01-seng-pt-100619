44d5a6c80c4223a87f394ef2fe144a63302b0f00
Wendy Harris <wendyv510@hotmail.com>
2019-10-15 21:39:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma=["kiwi"]
-  oxford_comma.join
-oxford_comma<< "durian"
-oxford_comma<< "starfruit"
-oxford_comma<< "mangos","dragonfruit"
-oxford_comma<< "lynchees", "pomelos"
+   oxford_comma<< "durian"
+   oxford_comma<< "starfruit"
+   oxford_comma<< "mangos","dragonfruit"
+   oxford_comma<< "lynchees", "pomelos"
+      oxford_comma.join
 end
